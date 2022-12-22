38a46946f7e8fe7e20220ddc075ef960333e5b54
Justin Spies <justin@nextup.ai>
2022-12-22 14:59:58 -0500

    (feature) DEV-680: allow roles to assume themselves, however this creates a chicken/egg issue... :/

--- federation.tf
+++ federation.tf
@@ -3,6 +3,7 @@ module "nextup_security_account_administrators" {
   source = "../modules/federation-role"
 
   name                 = "NextupSecurityAdministrators"
+  account_id           = var.aws_account_id
   source_account       = var.nextup_security_account_number
   attached_policy      = "arn:aws:iam::aws:policy/AdministratorAccess"
   max_session_duration = 43200
